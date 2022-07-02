a932622b46bb1b1ba5a66614868e6144419b920a
huangjianqin <459564034@qq.com>
2022-07-02 20:11:45 +0800

    MD5工具类

---
+++ src/test/java/org/kin/framework/utils/MD5Test.java
@@ -0,0 +1,11 @@
+package org.kin.framework.utils;
+
+/**
+ * @author huangjianqin
+ * @date 2022/7/2
+ */
+public class MD5Test {
+    public static void main(String[] args) {
+        System.out.println(MD5.common().encode("kin1234567890"));
+    }
+}
