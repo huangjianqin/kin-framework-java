37805cf3080c885780feedf5f1c2b2cfebf3cba7
huangjianqin <459564034@qq.com>
2022-09-04 23:33:19 +0800

    注释

--- src/main/java/org/kin/framework/concurrent/HashedWheelTimer.java
+++ src/main/java/org/kin/framework/concurrent/HashedWheelTimer.java
@@ -37,9 +37,9 @@ import java.util.concurrent.atomic.AtomicLong;
 /**
  * 1. wheel timer slot: 链表结构, O(1), 相比, {@link java.util.concurrent.DelayQueue}和{@link java.util.Timer}, O(log n)
  *      插入和删除调度任务的性能更高.
- * 2. {@link #newTimeout(TimerTask, long, TimeUnit)}并不会把task插入wheel timer slot
- * 3. 每个wheel timer带一个worker, 其核心逻辑如下:
- *      1. {@link Worker#processCancelledTasks()}, 将cancelled task从wheel timer slot上移除. 由此看出, cancel task并不会马上从wheel timer slot移除, 而已缓存在一个queue里面
+ * 2. {@link #newTimeout(TimerTask, long, TimeUnit)}并不会把task插入wheel timer slot, 而缓存在一个queue里面, 等待tick才处理
+ * 3. 每个wheel timer带一个worker, 模拟时钟tick, 其核心逻辑如下:
+ *      1. {@link Worker#processCancelledTasks()}, cancel task并不会马上从wheel timer slot移除, 而是缓存在一个queue里面, 此时才将cancelled task真正从wheel timer slot上移除
  *      2. 寻找本次tick的slot, 即{@link HashedWheelBucket}
  *      3. {@link Worker#transferTimeoutsToBuckets()}, 将new task插入wheel timer slot链表中, 并且硬编码写死最多处理10000个new task. 由此看出, 如果延迟task很多的话, task过期处理可能并不`及时`
  *      4. {@link HashedWheelBucket#expireTimeouts(long)}, 遍历所有task, 移除过期task并执行相对expire逻辑和移除cancelled task.
