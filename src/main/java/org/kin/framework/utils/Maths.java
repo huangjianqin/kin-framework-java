76d599ce8a08fb99cdaac6358bff561e2d43d24b
huangjianqin <459564034@qq.com>
2024-06-23 11:40:01 +0800

    补充2^n工具方法

--- src/main/java/org/kin/framework/utils/Maths.java
+++ src/main/java/org/kin/framework/utils/Maths.java
@@ -23,22 +23,104 @@ public final class Maths {
     }
 
     /**
-     * 判断{@code val}是否是2的N次方
+     * 判断{@code val}是否是2^N次方
      */
     public static boolean isPower2(int val) {
         return (val & -val) == val;
     }
 
     /**
-     * 最接近的2的n次方值
+     * 最接近且与大于等于{@code val}的2^n次方整形
      */
     public static int round2Power2(int val) {
-        if (val > 1073741824) {
-            throw new IllegalArgumentException("There is no larger power of 2 int for val:" + val + " since it exceeds 2^31.");
+        //2^30=1073741824
+        if (val > (1 << 30)) {
+            throw new IllegalArgumentException(String.format("no larger power of 2 integer for %d since it exceeds 2^30", val));
         } else if (val < 0) {
-            throw new IllegalArgumentException("Given val:" + val + ". Expecting val >= 0.");
+            throw new IllegalArgumentException("expect positive integer");
         } else {
+            if (val <= 2) {
+                return val;
+            }
+
             return 1 << 32 - Integer.numberOfLeadingZeros(val - 1);
         }
+    }
+
+    /**
+     * 最接近且与大于等于{@code val}的2^n次方整形
+     */
+    public static int ceil2Power2(int val) {
+        return round2Power2(val);
+    }
+
+    /**
+     * 最接近且与小于等于{@code val}的2^n次方整形
+     */
+    public static int floor2Power2(int val) {
+        if (val < 0) {
+            throw new IllegalArgumentException("expect positive integer");
+        }
+
+        if (val <= 2) {
+            return val;
+        }
+
+        return Integer.highestOneBit(val);
+    }
+
+    /**
+     * 判断{@code val}是否是2^N次方
+     */
+    public static boolean isPower2(long val) {
+        return (val & -val) == val;
+    }
+
+    /**
+     * 最接近且与大于等于{@code val}的2^n次方整形
+     */
+    public static long round2Power2(long val) {
+        //2^62=4611686018427387904
+        if (val > 4611686018427387904L) {
+            throw new IllegalArgumentException(String.format("no larger power of 2 integer for %d since it exceeds 2^62", val));
+        } else if (val < 0) {
+            throw new IllegalArgumentException("expect positive long");
+        } else {
+            if (val <= 2) {
+                return val;
+            }
+
+            // 如果是已经是2^n, 则-1
+            val--;
+            val |= val >> 1;
+            val |= val >> 2;
+            val |= val >> 4;
+            val |= val >> 8;
+            val |= val >> 16;
+            val |= val >> 32;
+            return val + 1;
+        }
+    }
+
+    /**
+     * 最接近且与大于等于{@code val}的2^n次方整形
+     */
+    public static long ceil2Power2(long val) {
+        return round2Power2(val);
+    }
+
+    /**
+     * 最接近且与小于等于{@code val}的2^n次方整形
+     */
+    public static long floor2Power2(long val) {
+        if (val < 0) {
+            throw new IllegalArgumentException("expect positive long");
+        }
+
+        if (val <= 2) {
+            return val;
+        }
+
+        return Long.highestOneBit(val);
     }
 }
--- src/test/java/org/kin/framework/utils/MathsTest.java
+++ src/test/java/org/kin/framework/utils/MathsTest.java
@@ -6,11 +6,54 @@ package org.kin.framework.utils;
  */
 public class MathsTest {
     public static void main(String[] args) {
-        System.out.println(Maths.isPower2(4));
+        System.out.println("---------------------------log2");
         System.out.println(Maths.log2(1025));
         System.out.println(Maths.log2(1024));
         System.out.println(Maths.log2(1023));
+
+        System.out.println("---------------------------log");
         System.out.println(Maths.log(5, 1));
         System.out.println(Maths.log(5, 25));
+
+        System.out.println("---------------------------isPower2");
+        System.out.println(Maths.isPower2(4));
+
+        System.out.println("---------------------------ceil2Power2");
+        System.out.println(Maths.ceil2Power2(1));
+        System.out.println(Maths.ceil2Power2(2));
+        System.out.println(Maths.ceil2Power2(3));
+        System.out.println(Maths.ceil2Power2(4));
+        System.out.println(Maths.ceil2Power2(500));
+        System.out.println(Maths.ceil2Power2(1000));
+//        System.out.println(Maths.ceil2Power2(Integer.MAX_VALUE));
+
+        System.out.println("---------------------------floor2Power2");
+        System.out.println(Maths.floor2Power2(1));
+        System.out.println(Maths.floor2Power2(2));
+        System.out.println(Maths.floor2Power2(4));
+        System.out.println(Maths.floor2Power2(5));
+        System.out.println(Maths.floor2Power2(515));
+        System.out.println(Maths.floor2Power2(1025));
+        System.out.println(Maths.floor2Power2(Integer.MAX_VALUE));
+
+        System.out.println("---------------------------ceil2Power2(long)");
+        System.out.println(Maths.ceil2Power2(1L));
+        System.out.println(Maths.ceil2Power2(2L));
+        System.out.println(Maths.ceil2Power2(3L));
+        System.out.println(Maths.ceil2Power2(4L));
+        System.out.println(Maths.ceil2Power2(500L));
+        System.out.println(Maths.ceil2Power2(1000L));
+        System.out.println(Maths.ceil2Power2(Long.valueOf(Integer.MAX_VALUE)));
+//        System.out.println(Maths.ceil2Power2(Long.MAX_VALUE));
+
+        System.out.println("---------------------------floor2Power2(long)");
+        System.out.println(Maths.floor2Power2(1L));
+        System.out.println(Maths.floor2Power2(2L));
+        System.out.println(Maths.floor2Power2(4L));
+        System.out.println(Maths.floor2Power2(5L));
+        System.out.println(Maths.floor2Power2(515L));
+        System.out.println(Maths.floor2Power2(1025L));
+        System.out.println(Maths.floor2Power2(Long.valueOf(Integer.MAX_VALUE)));
+        System.out.println(Maths.floor2Power2(Long.MAX_VALUE));
     }
 }
